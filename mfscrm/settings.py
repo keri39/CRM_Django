9738778a4fcba76b388db1ccce0d60c72a5de357
Abhishek Keri <111935093+keri39@users.noreply.github.com>
2022-10-15 21:56:59 -0500

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -57,8 +57,8 @@ ROOT_URLCONF = 'mfscrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')]
+#         'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         
         ,
         'APP_DIRS': True,
